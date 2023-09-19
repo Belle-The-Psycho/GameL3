8e44a11415bd0a124bf367f387f228141a321794
NASA <80649734+Belle-The-Psycho@users.noreply.github.com>
2023-09-19 13:16:29 +1200

    added hit detection

--- javascript.js
+++ javascript.js
@@ -9,7 +9,12 @@ function jump(){
     },500);
 }
 
-var checkDead = (setInterval(){
+var checkDead = setInterval(function(){
     var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(blockLeft<20 && blockLeft>0 && characterTop>=130){
+        block.style.animation = "none"
+        block.style.display = "none"
+        alert("You lose.");
+    }
 },10);
