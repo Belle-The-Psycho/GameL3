a25c8ab0b19331b39332ce4fcfd09b4d69314bd5
NASA <80649734+Belle-The-Psycho@users.noreply.github.com>
2023-09-19 12:55:17 +1200

    variable checkdead

--- javascript.js
+++ javascript.js
@@ -1,6 +1,14 @@
-var charactet =
-document.getElementById("character");
+var charactet = document.getElementById("character");
 var block = document.getElementById("block");
 function jump(){
-    character.classList.add("animate"); 
+    if(character.classList != "animate"){
+        character.classList.add("animate"); 
+    }
+    setTimeout(function(){
+        character.classList.remove("animate");
+    },500);
 }
+
+var checkDead = (setInterval(){
+    window.getComputedStyle(character).getPropertyValue("top");
+},10);
